9f3de30e2d3e04621361e4670d0b409796afeacc
Conrad <cdf2007@t-online.de>
2025-06-08 15:15:02 +0200

    Initialization of Electron

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu } = require('electron/main')
+const path = require('node:path')
+require('electron-reload')(__dirname)
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('main.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
